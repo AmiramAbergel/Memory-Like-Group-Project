c63b63648101282172db497f630bebd9147734b0
unknown <Amiram.ab@gmail.com>
2022-11-21 13:19:22 +0200

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Routes, Route, Navigate } from 'react-router-dom';
+import LandPage from './components/LandPage';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <main>
+                <Routes>
+                    <Route
+                        path='/'
+                        element={<Navigate replace to='/home'></Navigate>}
+                    />
+                    <Route path='/home' element={<LandPage />} />
+                </Routes>
+            </main>
+        </div>
+    );
 }
 
 export default App;
--- src/components/LandPage.js
+++ src/components/LandPage.js
@@ -1,1 +1,5 @@
 import React from 'react';
+
+const LandPage = () => {};
+
+export default LandPage;
