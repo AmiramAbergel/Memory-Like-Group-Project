6ff87ff064c6eda21d49751f89dcb42cf20d4fa9
Meir J Cohen <meir84@gmail.com>
2022-11-21 19:01:47 +0200

    After router and page merge - Amiram

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import LandPage from "./components/LandPage";
 import About from "./components/About";
+import Cards from "./components/Cards";
 
 function App() {
   return (
     <div className="App">
-      <main>
+      <header className="App-header">
         <Routes>
           <Route path="/" element={<Navigate replace to="/home"></Navigate>} />
           <Route path="/home" element={<LandPage />} />
-          <Route path="/home" element={<LandPage />} />
+          <Route path="/new-memory-game" element={<Cards />} />
           <Route path="/about" element={<About />} />
         </Routes>
-      </main>
+      </header>
     </div>
   );
 }
