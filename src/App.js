cb8cd5b5ce52a275e697fdbfce350a3d254d3b8d
Meir J Cohen <meir84@gmail.com>
2022-11-21 23:38:56 +0200

    Before readme edit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import LandPage from "./components/LandPage";
 import About from "./components/About";
-import Cards from "./components/Cards";
-import Table from "./table/Table";
+// import Cards from "./components/Cards";
+// import Table from "./table/Table";
 import GameBoard from "./components/GameBoard";
 // =============================================
 function App() {
