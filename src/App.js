3ee6ab668d4bcaa53fa0ae9adeb40a9c9170a9da
unknown <Amiram.ab@gmail.com>
2022-11-21 21:37:45 +0200

    comments removed

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
-import "./App.css";
-import { Routes, Route, Navigate } from "react-router-dom";
-import LandPage from "./components/LandPage";
-import About from "./components/About";
-import Cards from "./components/Cards";
-import Table from "./table/Table";
-import GameBoard from "./components/GameBoard";
+import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import LandPage from './components/LandPage';
+import About from './components/About';
+import GameBoard from './components/GameBoard';
 // =============================================
 function App() {
-  /*
-const initialState = {
-  wins: 0,
-  loses: 0,
-  totalScore: 0,
-  timer: 0,
-  playerWon: false,
-  playerLost: false,
-  sound: false,
-};
-*/
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/home"></Navigate>} />
-          <Route path="/home" element={<LandPage />} />
-          <Route path="/new-memory-game" element={<GameBoard />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <Routes>
+                    <Route
+                        path='/'
+                        element={<Navigate replace to='/home'></Navigate>}
+                    />
+                    <Route path='/home' element={<LandPage />} />
+                    <Route path='/new-memory-game' element={<GameBoard />} />
+                    <Route path='/about' element={<About />} />
+                </Routes>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,16 +1,27 @@
-import React from "react";
-import Cards from "./Cards";
-import Table from "../table/Table";
-import "./GameBoard.css";
+import React from 'react';
+import Cards from './Cards';
+import Table from '../table/Table';
+import './GameBoard.css';
 function GameBoard() {
-  return (
-    <div>
-      <div className="gameBoardContainer">
-        <Table />
-        <Cards />
-      </div>
-    </div>
-  );
+    /*
+const initialState = {
+  wins: 0,
+  loses: 0,
+  totalScore: 0,
+  timer: 0,
+  playerWon: false,
+  playerLost: false,
+  sound: false,
+};
+*/
+    return (
+        <div>
+            <div className='gameBoardContainer'>
+                <Table />
+                <Cards />
+            </div>
+        </div>
+    );
 }
 
 export default GameBoard;
