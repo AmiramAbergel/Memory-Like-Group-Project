088f0ee41871f797e633d0f5102034d80c713fe1
unknown <Amiram.ab@gmail.com>
2022-11-21 13:29:00 +0200

    Abot page and Button component added

--- src/components/LandPage.js
+++ src/components/LandPage.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
-const LandPage = () => {};
+const LandPage = () => {
+    return (
+        <div>
+            <header>
+                <h1>Welcome To Memory Like!</h1>
+            </header>
+            <main>
+                <h2>Description:</h2>
+                <p>This is flip card game that work on your memory</p>
+                <p>For more details go to about page...</p>
+            </main>
+            <footer>
+                <h2>Here is our options:</h2>
+                <button>Start New Game</button>
+                <button>About&Credits</button>
+            </footer>
+        </div>
+    );
+};
 
 export default LandPage;
