022559b3d5d0b89686c9c4532286f500c792d926
unknown <Amiram.ab@gmail.com>
2022-11-21 14:13:44 +0200

    Abot page done with routes

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,37 @@
 import React from 'react';
-
-const About = () => {};
+import { NavLink } from 'react-router-dom';
+const About = () => {
+    return (
+        <div>
+            <header>
+                <h1>About/Credits</h1>
+            </header>
+            <main>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged. It was popularised in the 1960s with
+                    the release of Letraset sheets containing Lorem Ipsum
+                    passages, and more recently with desktop publishing software
+                    like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+            </main>
+            <footer>
+                <button>
+                    <NavLink
+                        style={{ textDecoration: 'none', color: 'white' }}
+                        to={`/home`}
+                    >
+                        Back to main menu
+                    </NavLink>
+                </button>
+            </footer>
+        </div>
+    );
+};
 
 export default About;
